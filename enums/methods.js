94459006c0773a5efa14324b2a8b0dd67972457d
Alex Bardanov <dnbard@gmail.com>
2015-02-10 21:30:43 +0200

    disable not implemented methods

--- enums/methods.js
+++ enums/methods.js
@@ -1,3 +1,3 @@
 define(function(require, exports, module){
-    module.exports = [ 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'COPY', 'HEAD', 'OPTIONS', /*'LINK', 'UNLINK',*/ 'PURGE' ];
+    module.exports = [ 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'COPY', 'HEAD', 'OPTIONS', 'PURGE' ];
 });
